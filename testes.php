817b68663a8a259a6cb3a8e965cf436c4d93bb64
Márcio Diniz <marcio.sccp10@gmail.com>
2024-04-22 20:45:20 -0300

    Create testes.php

---
+++ testes.php
@@ -0,0 +1,21 @@
+<?php 
+    $nome = "Márcio";
+    $sobrenome = "Diniz";
+
+    const PAIS = "Brasil";
+
+    //
+    //
+    //
+    //
+
+    //
+
+    // PAIS = "EUA";
+
+    //
+    //
+    //
+    //
+
+    echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
