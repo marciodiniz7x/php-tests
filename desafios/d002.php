42aa39a2be4dd0a52e2bdf37bc4b555031fc4137
Márcio Diniz <marcio.sccp10@gmail.com>
2024-04-26 16:22:55 -0300

    Updates

---
+++ desafios/d002.php
@@ -0,0 +1,4 @@
+<?php
+$numeroAleatorio = rand(0, 100);
+echo "Gerando número aleatório entre 0 e 100...
+<p>O valor gerado foi <strong>$numeroAleatorio</strong></p>";
--- desafios/index.php
+++ desafios/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Desafios</title>
 </head>
+
 <body>
     <h1>Desafios</h1>
-    <section class="desafio001">
+    <section class="desafios desafio001">
         <h1>Desafio 01</h1>
 
         <div>
@@ -17,23 +19,22 @@
                     <span>Informe um número:</span>
                 </label>
                 <input type="number" name="numero" id="numero">
-                <input type="submit" value="Calcular" id="calcular">
+                <input type="submit" value="Calcular" id="calcular" class="submits">
             </form>
         </div>
     </section>
 
-    <section class="desafio002">
+    <section class="desafios desafio002">
         <h1>Desafio 02</h1>
-
-        <div>
-            <form action="d002.php" method="get">
-                <label for="numero">
-                    <span>Gerando número aleatório entre 0 e 100</span>
-                </label>
-                
-                <input type="submit" value="Calcular" id="calcular">
-            </form>
-        </div>
+           
+        <?php
+        include 'd002.php';
+        ?>
+           
+        <a href="index.php" class="submits">Gerar Outro</a>
+        
     </section>
+
 </body>
+
 </html>
