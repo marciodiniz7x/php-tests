353e3f881c0f0c651e6da5bbc6bf07d56c039df2
Márcio Diniz <marcio.sccp10@gmail.com>
2024-04-25 21:40:23 -0300

    Updates

--- d001/cad.php
+++ d001/cad.php
@@ -10,13 +10,23 @@
     <header>
         <h1>Resultado Final</h1>
     </header>
-    <section>
+    <main class="resultado">
+        <div>
         <?php 
             $numero = $_GET["numero"] ?? "Inválido";
+            $antecessor = $numero - 1;
+            $sucessor = $numero + 1;
 
-            echo "<p> O número escolhido foi $numero! </p>"
+            echo "
+            <p> O número escolhido foi <strong>$numero</strong></p>
+
+            <p> O seu antecessor é <strong>$antecessor</strong></p>
+
+            <p> O seu sucessor é <strong>$sucessor</strong> </p>
+            "
         ?>
+        </div>
         <a href="javascript:history.go(-1)">&#8592; Voltar aos desafios</a>
-    </section>
+    </main>
 </body>
 </html>
